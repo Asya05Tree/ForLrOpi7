89541d01b7bc2504d59634c14735927ce64f0cf2
Nasya_K <nastenka05m@gmail.com>
2022-11-14 12:00:45 +0200

    int i --> int k

--- min a in array.cpp
+++ min a in array.cpp
@@ -1,6 +1,4 @@
 // min a in array.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡
-
 #include <iostream>
 #include <ctime>
 using namespace std;
@@ -14,14 +12,14 @@ int main()
     for (int i = 0; i < SIZE; i++)
     {
         int RandValue = rand() % 20;
-        arr[i] = RandValue; //‚ÓÚ ÚÛÚ ·˚ÎÓ == ‚ÏÂÒÚÓ =, ÍÓÏÔÂÎˇÚÓÛ ÌÓÏ
+        arr[i] = RandValue; 
     }
     int minValue = arr[0];
-    for (int i = 1; i < SIZE; i++)
+    for (int k = 1; k < SIZE; k++)
     {
         if (arr[i] < minValue)
         {
-            minValue = arr[i];
+            minValue = arr[k];
         }
     }
 
@@ -32,26 +30,3 @@ int main()
         cout << arr[i] << endl;
     }
 } // ˇ ÒÏÓ„Î‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡‡
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
